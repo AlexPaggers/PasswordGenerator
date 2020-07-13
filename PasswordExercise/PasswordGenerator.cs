42bffc612d3bc30fc22f7541d350211d164f0df6
Alex Pagliarini <alex.pagliarini@gmail.com>
2020-07-13 18:34:37 +0100

    Added to git

---
+++ PasswordExercise/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PasswordExcercise
+{
+	public class PasswordRequirements
+	{
+		public int MaxLength { get; set; }
+		public int MinLength { get; set; }
+		public int MinUpperAlphaChars { get; set; }
+		public int MinLowerAlphaChars { get; set; }
+		public int MinNumericChars { get; set; }
+		public int MinSpecialChars { get; set; }
+	}
+
+	public interface IPasswordGenerator
+	{
+		string GeneratePassword(PasswordRequirements requirements);
+	}
+
+	public class PasswordGenerator : IPasswordGenerator
+	{
+		public string GeneratePassword(PasswordRequirements requirements)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
