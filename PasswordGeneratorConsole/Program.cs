49f76b7a073768adf05ae60dddb5d03f0f052539
Alex Pagliarini <alex.pagliarini@gmail.com>
2020-07-16 00:31:44 +0100

    Added console project

---
+++ PasswordGeneratorConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PasswordExcercise;
+
+namespace PasswordGeneratorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfGenerations = 50;
+
+            var generator = new PasswordGenerator();
+            var requirements = new PasswordRequirements(
+                minLength: 8,
+                maxLength: 16,
+                minLowerAlpha: 2,
+                minUpperAlpha: 2,
+                minNumeric: 2,
+                minSpecial: 2);
+
+            for (int i = 0; i < numberOfGenerations; i++)
+            {
+                Console.WriteLine(generator.GeneratePassword(requirements));
+            }
+        }
+    }
+}
